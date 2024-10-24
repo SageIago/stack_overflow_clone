2e9f050de9e42b5016b414c450235e53849654a9
Borngreat Ikwutah <iagocech2056@gmail.com>
2024-10-24 18:12:27 +0100

    Refactor ask-question page to include Questions component

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,13 @@
+import Questions from "@/components/Forms/Questions"
 
 const page = () => {
   return (
-    <div>Ask Questions</div>
+    <div>
+        <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Questions />
+      </div>
+    </div>
   )
 }
 
---
+++ lib/actions/validation.ts
@@ -0,0 +1,7 @@
+import * as z from "zod";
+
+export const QuestionsSchema = z.object({
+    title: z.string().min(5, {message: "Title must be at least five characters"}).max(130, {message: "Title must be less than 130 characters"}),
+    explanation: z.string().min(100, {message: "Explanation of the Problem must bre greater than 100 characters"}).max(500, {message: "Explanation of the Problem must be less than 500 characters"}),
+    tags: z.array(z.string().min(1).max(15)).min(1, {message: "Tags must not be empty"}).max(3, {message: "You can only add up to Three tags"}),
+  })
