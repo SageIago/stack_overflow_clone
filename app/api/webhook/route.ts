bc41119f7345f75412c221682245a97d39eb0a10
Borngreat Ikwutah <iagocech2056@gmail.com>
2024-10-29 12:18:17 +0100

    Deploying Webhooks Part2

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: Request) {
   // For this guide, you simply log the payload to the console
   const eventType = evt.type;
 
-  console.log({ eventType })
+  console.log({ eventType } + "This is the event payload....")
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
